b5aa5f121aaf3cc8f6e71b0116aabeaf24ef003a
AKASH KUMAR <akashkumarcs23@gmail.com>
2024-10-15 21:30:10 +0530

    first Commit

---
+++ LeetCode/LinkedList/ListNode.java
@@ -0,0 +1,11 @@
+package LeetCode.LinkedList;
+
+public class ListNode {
+    int val;
+    ListNode next;
+
+    public ListNode(int val) {
+        this.val = val;
+        this.next = null;
+    }
+}
